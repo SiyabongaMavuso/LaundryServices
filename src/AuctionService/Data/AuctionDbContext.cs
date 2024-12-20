b3ef9ed86af7697c79e061c4803519bbe3d6fcdc
siyabongamavuso <siyabonga.auo@gmail.com>
2024-12-20 23:09:00 +0200

    1st push, auction service created with models and dbcontext added

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Data;
+using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data;
+
+public class AuctionDbContext : DbContext
+{
+    public AuctionDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    public DbSet<Auction> Auctions { get; set; }
+
+
+}
